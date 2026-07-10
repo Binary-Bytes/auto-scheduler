d0fafc9a3568991055f47eb40c0cf184871189f7
Vihaan Sharmaa <88529771+Binary-Bytes@users.noreply.github.com>
2026-07-10 16:28:39 +0000

    refactor: update page title and icon

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="My App", page_icon="🏠", layout="wide")
+st.set_page_config(page_title="Homepage", page_icon="🗓️", layout="wide")
 st.title("🏠 Home")
 st.write("Visit any of the two pages from the sidebar to access the automatic schedulers! 😄")
